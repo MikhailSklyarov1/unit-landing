3239aa1f78998be23ee55572dbbc7dd7be26a4b0
Sklyarov Mikhail <msklyarov@gintell.ru>
2024-05-18 19:24:09 +0300

    Блок с кнопками записаться и задать вопрос

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function toggleMenu() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('active');
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
 }
+
+// Создание экземпляра карты.
+const map = new ymaps3.YMap(document.getElementById("YMapsID"), {
+  location: {
+    center: [37.64, 55.76],
+    zoom: 10,
+  },
+});
 
