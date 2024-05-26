0f532ecce133e16b3be0a7596006eac39982fcf0
Denis Levshin <levshin01@bk.ru>
2024-05-26 20:53:19 +0300

    update coord map1, del button control on all map

--- icon_customImage.js
+++ icon_customImage.js
@@ -1,17 +1,25 @@
 ymaps.ready(function () {
     var map1 = new ymaps.Map('map1', {
-        center: [48.708048, 44.516939],
-        zoom: 14
+        center: [48.716049, 44.525939],
+        zoom: 14,
+        controls:[]
+
+    }, {
+        suppressMapOpenBlock: true
+
     });
-    var placemark1 = new ymaps.Placemark([48.714070, 44.528339], {
+        var placemark1 = new ymaps.Placemark([48.714070, 44.528339], {
         balloonContent: 'Обучающий центр ЮНИТ (центральный район)'
     });
     map1.geoObjects.add(placemark1);
 
     var map2 = new ymaps.Map('map2', {
         center: [48.805724, 44.606654],
-        zoom: 14
+        zoom: 14,
+        controls:[]
     }, {
+        suppressMapOpenBlock: true
+
     });
     var placemark2 = new ymaps.Placemark([48.805724, 44.606654], {
         balloonContent: 'Обучающий центр ЮНИТ (тракторозаводской район)'
@@ -20,10 +28,18 @@ ymaps.ready(function () {
 
     var map3 = new ymaps.Map('map3', {
         center: [48.703418, 44.516026],
-        zoom: 14
-    }, );
+        zoom: 14,
+        controls:[]
+
+    }, {
+        suppressMapOpenBlock: true
+
+    });
     var placemark3 = new ymaps.Placemark([48.703418, 44.516026], {
         balloonContent: ''
     });
     map3.geoObjects.add(placemark3);
 });
+
+//controls[] - указать кнопки которые тебе нужны, если не указать их не будет... Верхних
+//suppressMapOpenBlock: true -  отключение нижних кнопок
