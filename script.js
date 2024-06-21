3c308b3585deda5a684963302be468f9ce8afed6
Sonya <johndoe@example.com>
2024-06-21 23:29:32 +0300

    аккордеон но без аниамций

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// Получаем все элементы details
+const accordion = document.querySelectorAll('details');
+
+// Проходимся по каждому элементу
+accordion.forEach(item => {
+
+  // На каждый элемент вешаем слушатель события клика
+  item.addEventListener('click', (e) => {
+
+    // Сбрасываем стандартное действие при клике
+    e.preventDefault();
+    
+    // Находим открытый элемент
+    const openItem = document.querySelector('details[open]');
+    
+    // Если есть открытый элемент удаляем ему атрибут open
+    if (openItem) openItem.open = false;
+
+    // Если открытый элемент не является тем, на который мы нажали, то нажатому элементу добавляем атрибут open
+    if (openItem !== item) item.open = true 
+  })
+})
